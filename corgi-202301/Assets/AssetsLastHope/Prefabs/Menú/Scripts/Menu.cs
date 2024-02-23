7f0efe5b311fe0b45161ec6bb2435c5aacda22ab
Palomo <ilpalomon@gmail.com>
2024-02-22 22:38:30 -0500

    BotonesMenu

---
+++ corgi-202301/Assets/AssetsLastHope/Prefabs/Menú/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+   public void OnClickJugar()
+    {
+        MMSceneLoadingManager.LoadScene("Nivel1");
+    }
+}
